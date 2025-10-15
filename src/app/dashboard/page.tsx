3b376cae1174d20b1c95edb0838ae552d8a1fa6f
Muhammad Rehan <mrehanirfan38@gmail.com>
2025-10-15 17:33:42 +0500

    some changes

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -7,11 +7,11 @@ function Dashboardpage() {
   return (
     <>
       <Navbar />
+     
       <div className="max-w-7xl mx-auto px-6 py-8 pt-24">
         <WelcomeSection />
         <MainActions />
         <ActivityOverview />
-        Dashboard
       </div>
     </>
   )
